c8be3c66fd290ddd3dbf37e9d99bc7a90f215696
wafaa909 <wafaa_gu90@hotmail.com>
2022-12-14 19:56:52 +0300

    done the LAST DAY :(

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ let random_grade_div = document.getElementById("random-grade");
 
 
 /*  الجزء الأول والثاني من التمرين */
+grades.pop();
+
+grades.push(99);
+
+grades.forEach((grade)=>(all_grades_div.innerHTML += `<span> ${grade}</span>`));
 
 
-
-
+let randomIndex = Math.floor(Math.random() * grades.length);
+random_grade_div.innerHTML = `<span>#${randomIndex}: ${grades[randomIndex]}<span>`
 
 
 
@@ -26,12 +31,19 @@ let course = {
 }
 
 //Change studentsCount,track property here
-
+course.track = " web"
+course.studentsCount = 40
 
 
 //Console log here
 
+console.Log("Hello my Name is Wafaa , i'm in "+ course.track + "at "+ course.name)
 
 
 //Create student object here
+
+let student = {
+  name: "Sara",
+  universty: "GUST"
+}
 
